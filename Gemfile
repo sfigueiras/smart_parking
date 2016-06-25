228dfa0e2eb5e78fac7e5f3a1a6c09e875cf8468
Santiago Figueiras <santi.figueiras95@gmail.com>
2016-06-24 21:35:27 -0300

    Added font-awesome-rails and redesigned area views to demonstrate easilly the protoype's functionallity

--- Gemfile
+++ Gemfile
@@ -25,7 +25,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'devise'
 gem 'materialize-sass'
-gem 'font-awesome-sass'
+gem 'font-awesome-rails'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- db/migrate/20160622000639_area_and_spots_preloaded_data.rb
+++ db/migrate/20160622000639_area_and_spots_preloaded_data.rb
@@ -1,11 +1,6 @@
 class AreaAndSpotsPreloadedData < ActiveRecord::Migration
-  areas = ['La Plata Norte', 'La Plata Sur', 'La Plata Oeste', 
-    'Berisso', 'Ensenada']
-  areas.each do |name|
-    say "Inserting ${area} area"
-    area = Area.create(name: name)
-    6.times do |number|
-      area.spots.create(number: number+1)
-    end
+  area = Area.create(name: 'La Plata Norte')
+  6.times do |number|
+    area.spots.create(number: number+1)
   end
 end
